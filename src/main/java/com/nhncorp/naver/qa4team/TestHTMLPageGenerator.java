3b951f7f8691f12ffcf041cc1155fface6d2ad00
unknown <nhn@.(none)>
2010-04-22 15:01:04 +0900

    Change html parser to jsoup from HtmlCleaner. &ampo bug is fixed.

--- src/main/java/com/nhncorp/naver/qa4team/TestHTMLPageGenerator.java
+++ src/main/java/com/nhncorp/naver/qa4team/TestHTMLPageGenerator.java
@@ -3,25 +3,17 @@ package com.nhncorp.naver.qa4team;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.TransformerFactoryConfigurationError;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
-import org.htmlcleaner.DomSerializer;
-import org.htmlcleaner.HtmlCleaner;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class TestHTMLPageGenerator {
 	
@@ -30,41 +22,28 @@ public class TestHTMLPageGenerator {
 		HttpClient client = new HttpClient();
 		client.getParams().setParameter("http.protocol.content-charset",
 				"UTF-8");
-		// client.getParams().setParameter("http.protocol.content-charset",
-		// "UTF-8");
 		GetMethod get = new GetMethod(url);
-		// get.addRequestHeader("Content-Type",
-		// FORM_URL_ENCODED_CONTENT_TYPE+";charset=UTF-8");
-		// get.addRequestHeader("Content-Type", "text/html; charset=UTF-8");
 		get.setFollowRedirects(true);
 		try {
 			client.executeMethod(get);
-			final InputStream in = get.getResponseBodyAsStream();
 			File f = new File(target);
 			OutputStream out = new FileOutputStream(f);
-			injectJavascript(in, out);
-			in.close();
+			String result = injectJavascript(get.getResponseBodyAsString());
+			out.write(result.getBytes());
 			out.close();
 		} catch (Exception e) {
 			throw new IllegalStateException(e);
 		}
 	}
+	
 
-	public void injectJavascript(final InputStream in, OutputStream out) throws ParserConfigurationException, IOException, TransformerFactoryConfigurationError, TransformerException {
-		HtmlCleaner cleaner = new HtmlCleaner();
-		DomSerializer dom = new DomSerializer(cleaner.getProperties());
-		
-		Document doc = dom.createDOM(cleaner.clean(in));
-		Node head = doc.getElementsByTagName("head").item(0);
+	public String injectJavascript(String data) throws ParserConfigurationException, IOException, TransformerFactoryConfigurationError, TransformerException {
+		Document doc = Jsoup.parse(data);
+		Element head = doc.getElementsByTag("head").get(0);
 		Element script = doc.createElement("script");
-		script.setAttribute("type", "text/javascript");
-		script.setAttribute("src", "snapsie.js");
+		script.attr("type", "text/javascript");
+		script.attr("src", "snapsie.js");
 		head.appendChild(script);
-		
-		Transformer transformer = TransformerFactory.newInstance().newTransformer();
-		transformer.setOutputProperty(OutputKeys.ENCODING, "euc-kr");
-		DOMSource source = new DOMSource(doc);
-		StreamResult sr = new StreamResult(out);
-		transformer.transform(source, sr);
+		return doc.html();
 	}
 }
--- src/test/java/com/nhncorp/naver/qa4team/HtmlDownloadTest.java
+++ src/test/java/com/nhncorp/naver/qa4team/HtmlDownloadTest.java
@@ -1,19 +1,14 @@
 package com.nhncorp.naver.qa4team;
 
 import java.io.File;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathFactory;
 
 import org.apache.commons.io.FileUtils;
-import org.htmlcleaner.DomSerializer;
-import org.htmlcleaner.HtmlCleaner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
 
 import static org.testng.Assert.*;
 
@@ -32,20 +27,14 @@ public class HtmlDownloadTest {
 
 	@Test
 	public void downloadHTMLTest() throws Exception {
-		// Logger logger = Logger.getLogger(this.getClass());
 		assertFalse((new File(target)).isFile());
 		TestHTMLPageGenerator generator = new TestHTMLPageGenerator();
 		generator
-				.generateTestHTMLPage("http://search.naver.com/search.naver?where=nexearch&query=%BE%C8%B3%E7", target);
-		assertTrue((new File(target)).isFile());
-		
-		HtmlCleaner cleaner = new HtmlCleaner();
-		DomSerializer dom = new DomSerializer(cleaner.getProperties());
-		Document doc = dom.createDOM(cleaner.clean(new File(target)));
-		XPath xpath = XPathFactory.newInstance().newXPath();
-		XPathExpression expr = xpath.compile("/html/head/script[@src='snapsie.js']");
-		Object result = expr.evaluate(doc, XPathConstants.NODESET);
-		NodeList nodes = (NodeList) result;
-		assertEquals(1, nodes.getLength());
+				.generateTestHTMLPage("http://search.naver.com/search.naver?where=nexearch&query=%C7%C1%B6%F3%C7%CF%C0%C7+%BF%AC%C0%CE", target);
+		File f = new File(target);
+		assertTrue(f.isFile());
+		Document doc = Jsoup.parse(FileUtils.readFileToString(f));
+		Elements elms = doc.select("script[src=snapsie.js]");
+		assertEquals(1, elms.size());
 	}
 }
